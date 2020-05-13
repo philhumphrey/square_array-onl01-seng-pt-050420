51e4066b1eeed300e726e557fc52a876ab1d345e
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-13 22:02:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 
 def square_array(new_numbers)
   new_numbers = []
-  new_numbers.each  |number| new_numbers << number ** 2
+  new_numbers.each  |number| numbers << number ** 2
 end
    new_numbers
  end
